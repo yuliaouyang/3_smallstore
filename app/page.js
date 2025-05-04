2ecbd0d4232a45eaa73647a6fc3bc000292c1ff5
yuliaouyang <poiyulia2000@gmail.com>
2025-05-05 01:42:02 +0900

    for deploy

--- app/page.js
+++ app/page.js
@@ -2,32 +2,41 @@ import ImageBanner from "@/components/ImageBanner";
 import Products from "@/components/Products";
 
 export async function getProducts() {
-  const baseURL = process.env.NEXT_PUBLIC_BASE_URL
-  const response = await fetch(baseURL + '/api/products')
-  const products = await response.json()
-  return products;
+    try {
+
+        const baseURL = process.env.NEXT_PUBLIC_BASE_URL
+        const response = await fetch(baseURL + '/api/products')
+        const products = await response.json()
+        return products
+    } catch (err) {
+        console.log(err.stack)
+        console.log(err.message)
+        return []
+    }
 }
 
 export default async function Home(props) {
-  const products = await getProducts();
+    const products = await getProducts()
 
-  let planner = null
-  let stickers = []
+    let planner = null
+    let stickers = []
 
-  for (let product of products) {
-    if (product.name === 'Medieval Dragon Month Planner') {
-        planner = product
-        continue
+
+    for (let product of products) {
+        if (product.name === 'Medieval Dragon Month Planner') {
+            planner = product
+            continue
+        }
+        stickers.push(product)
     }
-    stickers.push(product)
-}
 
-  return (
-    <>
-      <ImageBanner />
-      <section>
-        <Products planner={planner} stickers={stickers}/>
-      </section>
-    </>
-  );
+
+    return (
+        < >
+            <ImageBanner />
+            <section>
+                <Products planner={planner} stickers={stickers} />
+            </section>
+        </>
+    );
 }
